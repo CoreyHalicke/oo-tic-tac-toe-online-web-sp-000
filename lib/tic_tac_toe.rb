0bf587890fa6f660b25a9e3bc71749e059af5f28
Corey H <c.l.halicke@gmail.com>
2019-02-05 01:12:35 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,55 @@
+class TicTacToe
+ 
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+  
+   def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+  
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(index)
+    @index = index.to_i - 1
+  end
+  
+  def move(index, player = "X")
+    @board[index] = player
+  end 
+  
+  def position_taken?(index)
+    !(@board[index].nil? || @board[index] == " ")
+  end
+  
+  def valid_move?(index)
+    index.between?(0,8) && @board[index] != "X" && @board[index] != "O"
+    if index.between?(0,8) && @board[index] != "X" && @board[index] != "O"
+      return TRUE
+    end
+  end
+  
+  def turn_count
+    counter = 0 
+    @board.each do |spot_taken|
+      if spot_taken == "X" || spot_taken == "O"
+        counter += 1
+      end
+    end
+  return counter
+  end
+  
+end
